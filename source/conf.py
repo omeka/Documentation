30df2e92469023589cb958d409a6ac9780bb5b75
John Flatness <john@zerocrates.org>
2014-06-05 18:14:16 -0400

    Try to Omeka-ify things a little.

--- source/conf.py
+++ source/conf.py
@@ -120,8 +120,7 @@ html_theme = u'default'
 # documentation.
 #html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_templates']
+templates_path = ['_templates']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
